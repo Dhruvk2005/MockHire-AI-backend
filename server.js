703671794945fde5bb262e04b6e43c91acb87864
Dhruv Khade <dhruvkhade05@gmail.com>
2026-05-09 16:41:00 +0530

    doene

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express")
+const app =  express()
+const PORT = 5000
+
+app.get("/",(req,res)=>{
+    res.send("helloo world")
+})
+
+app.listen(PORT,()=>{
+    console.log("server is running at 5000 ")
+})
